e474e8271f1dce4c7f1126863e301752f6321052
Matthieu ENGLES <englesmatthieu@gmail.com>
2022-09-01 17:18:54 +0200

    Ajout MVC et creation logger

---
+++ src/projet_3.py
@@ -0,0 +1,25 @@
+from lib.molp_log import Logger
+
+log = Logger('OCR3', verbose=True)
+
+def try_except(function,log=log):
+
+    def wrapper(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+        except Exception as _e:
+            log.error('',args,_e,func_name=function.__name__)
+            result = None
+        finally:         
+            return result
+ 
+    return wrapper
+
+@try_except
+def divide(a,b):
+    return (a/b)
+
+print(divide(10,0))
+
+
+print(log.log_var)
